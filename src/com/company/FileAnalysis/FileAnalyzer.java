6c1305d9f85dd1cdfb68ceec7aaf2afe03b29ce8
mkoww <mbogucki@metal.agh.edu.pl>
2019-11-13 22:10:22 +0100

    firstone

---
+++ src/com/company/FileAnalysis/FileAnalyzer.java
@@ -0,0 +1,26 @@
+package com.company.FileAnalysis;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class FileAnalyzer {
+    public static List<Path> getFilesList() {
+        try {
+            Stream<Path> path = Files.walk(Paths.get(System.getProperty("user.dir")));
+            List<Path> list = path
+                    .filter(files -> files.toString().contains(".java"))
+                    .collect(Collectors.toList());
+            return list;
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,15 @@
 package com.company;
 
+import com.company.FileAnalysis.FileAnalyzer;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        List<Path> files;
+        files = FileAnalyzer.getFilesList();
     }
 }
