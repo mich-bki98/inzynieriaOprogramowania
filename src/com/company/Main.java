fb8e5f4daca94f4fd1a2de09ef28f2b1b0d1814d
Leaf <leaftheplayer@gmail.com>
2019-11-14 15:30:04 +0100

    Dodawanie grafów ale nie działa przez jakiś bug że nie ma jakiegos slf4j

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,6 +2,7 @@ package com.company;
 
 import com.company.fileAnalysis.FileAnalyzer;
 import com.company.model.FileDetails;
+import graphs.ClassRelationGraph;
 
 import java.io.IOException;
 import java.nio.file.Path;
@@ -9,6 +10,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
+
+    private static void generateGraphs(List<FileDetails> filesList){ //Tutaj będą wywołania funkcji rysowania grafów, na razie jest historyjka 1 więc tylko jeden graf
+
+        ClassRelationGraph crelGraph = new ClassRelationGraph();
+        crelGraph.createClassRelationGraph(filesList);
+    }
 
     public static void main(String[] args) {
         List<Path> files;
@@ -23,7 +30,7 @@ public class Main {
         }
 
         FileAnalyzer.analyzeDependencies(filesList);
+       generateGraphs(filesList);
 
-        System.out.println("xxxx");
     }
 }
